88e2b519e3275f2d11c19be183af516fde9df445
Sarveshwar Kumar Shukla <sarveshwarkumarshukla@gmail.com>
2025-07-01 02:09:40 +0530

    update base url

--- src/utils/constansts.jsx
+++ src/utils/constansts.jsx
@@ -1,3 +1,4 @@
-// const BASE_URL = "http://localhost:7777";
-const BASE_URL = "https://devtinder-2-ocsm.onrender.com";
+const BASE_URL = "/api";
 export default BASE_URL;
+// const BASE_URL = "http://localhost:7777";
+// const BASE_URL = "https://devtinder-2-ocsm.onrender.com";
